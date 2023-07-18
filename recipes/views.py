59e14e8c1b73605ce7367541846eb32ec41af3dc
João Neto <jbfaneto@gmail.com>
2023-07-17 22:28:12 -0300

    preparing the database

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from utils.recipes.factory import make_recipe
 from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+   return render(request, 'recipes/pages/home.html', context={
+       'recipes': [make_recipe() for _ in range(19)]
+   })
     
-
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
     
